b3bd8e310f6d6de544926f3e0c4d60ecc2b8fc14
raindrop-aqua <raindrop.aqua@gmail.com>
2014-03-06 20:04:34 +0900

    first commit

---
+++ Chapter01-GettingStarted/src/main/java/com/prism7/refactoring/step2/Customer.java
@@ -0,0 +1,65 @@
+package com.prism7.refactoring.step2;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+
+	private String name;
+	private Vector<Rental> rentals = new Vector<Rental>();
+
+	public Customer(String name) {
+		this.name = name;
+	}
+
+	public void addRental(Rental arg) {
+		rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRentarPoints = 0;
+		Enumeration<Rental> rentals = this.rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+		
+		// １行ごとに金額を計算
+		while (rentals.hasMoreElements()) {
+			double thisAmount = 0;
+			Rental each = rentals.nextElement();
+
+			switch (each.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2) {
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+					break;
+				}
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3) {
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				}
+			}
+			// レンタルポイントを加算
+			frequentRentarPoints++;
+			// 新作を２日以上借りた場合はボーナスポイント
+			if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1) {
+				frequentRentarPoints++;
+			}
+			
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+			totalAmount += thisAmount;
+		}
+		// フッタ部分の追加
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRentarPoints) + " freqent renter points";
+		return result;
+	}
+}
